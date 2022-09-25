512da9687166a6932f2624547b3d05a1560a1194
Rj123 <95161695+Raynould-Joseph@users.noreply.github.com>
2022-09-25 23:12:23 +0530

    reedit

--- app/src/main/java/com/example/actionbar/MainActivity.java
+++ app/src/main/java/com/example/actionbar/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.actionbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuAdapter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,16 +20,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView lefticon=findViewById(R.id.left_icon);
-        ImageView righticon=findViewById(R.id.right_icon);
-        TextView text= findViewById(R.id.title);
+        ImageView lefticon = findViewById(R.id.left_icon);
+        ImageView righticon = findViewById(R.id.right_icon);
+        TextView text = findViewById(R.id.title);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater=getMenuInflater();
-        inflater.inflate(R.menu.menu,menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.add:
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                break;
+            case R.id.exit:
+                finish();
+                break;
+            case R.id.share:
+                startActivity(new Intent(MainActivity.this, share_activity.class));
+                break;
+
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
